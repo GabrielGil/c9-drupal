abe0012755f47932e1a9df7f89b4cf04b40c0442
Gabriel <gabriel@gabrielgil.es>
2015-06-11 13:32:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ exec 2>&1
 
 # Install Drush
 # From Composer
-echo -en "\032[35m Installing/updating Drush...\n"
+echo -en "\033[35m Installing/updating Drush...\n"
 composer global require drush/drush:7.*
 
 # Add to $PATH
 if grep -q 'export PATH="$HOME/.composer/vendor/bin:$PATH"' $HOME/.bashrc
 then
     # code if found
-    echo -en "\032[36m Composer vendor path is already included.\n"
+    echo -en "\033[36m Composer vendor path is already included.\n"
 else
     # code if not found
     echo -en "\033[33m Composer vendor path is not included yet.\n"
@@ -24,11 +24,11 @@ else
     echo -en "\033[33m Done!\n"
 fi
 
-echo -en "\032[35m Reloading source..\n"
+echo -en "\033[35m Reloading source..\n"
 # Reload source
 source $HOME/.bashrc
 
 # Copy Runner
-echo -en "\032[35m Get and copy runner...\n"
+echo -en "\033[35m Get and copy runner...\n"
 mkdir -p $HOME/workspace/.c9/runners
 sudo wget https://raw.githubusercontent.com/GabrielGil/c9-drupal/master/Drupal7.run --output-document=$HOME/workspace/.c9/runners/Drupal7.run
