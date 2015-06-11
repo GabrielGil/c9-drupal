aa7e5903faed7e6aa792e832a6d1c364fbe3063c
Gabriel <gabriel@gabrielgil.es>
2015-06-11 10:14:23 +0200

    Update installer.sh
    Avoids export Drush to the path more than once.

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 
-
 # Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
 exec > >(tee /tmp/installlog.txt)
 # Without this, only stdout would be captured
 exec 2>&1
 
 
-
 # Install Drush
-
-# From composer
+# From Composer
 composer global require drush/drush:7.*
 # Add to $PATH
-echo 'export PATH="$HOME/.composer/vendor/bin:$PATH"' >> $HOME/.bashrc
+if grep -q 'export PATH="$HOME/.composer/vendor/bin:$PATH"' $HOME/.bashrc
+then
+    # code if found
+else
+    # code if not found
+    echo 'export PATH="$HOME/.composer/vendor/bin:$PATH"' >> $HOME/.bashrc
+fi
 # Reload source
 source $HOME/.bashrc
-
 
 # Copy Runner
 sudo wget https://raw.githubusercontent.com/GabrielGil/c9-drupal/master/Drupal7.run --output-document=$HOME/workspace/.c9/runners/Drupal7.run
